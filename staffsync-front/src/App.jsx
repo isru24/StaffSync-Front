994412c03aba94bb2145247f34629b5d8297bba8
Israel <israelmandefro165@gmail.com>
2026-04-11 13:22:24 +0300

    modified:   staffsync-front/src/App.jsx

--- staffsync-front/src/App.jsx
+++ staffsync-front/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import { Route, Routes} from 'react-router-dom';
 import './App.css'
 import Signup from './screens/signup';
+import Login from './screens/login';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main>
       <Routes>
         <Route path='/' element = {<Signup/>}/>
+        <Route path='/Login' element = {<Login/>}/>
       </Routes>
     </main>
   )
