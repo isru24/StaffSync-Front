7d7b519c8cf84e9d230aebd3dd3eedee6c00e925
Israel <israelmandefro165@gmail.com>
2026-04-14 13:08:48 +0300

    modified:   staffsync-front/src/App.jsx

--- staffsync-front/src/App.jsx
+++ staffsync-front/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Signup from './screens/signup';
 import Login from './screens/login';
+import Navbar from './screens/Navbar';
+import Dashboard from './screens/Dashboard';
+import AuthLayout from './routes/Authlayout';
+import MainLayout from './routes/Mainlayout';
 
 function App() {
 
   return (
     <main>
       <Routes>
-        <Route path='/' element = {<Signup/>}/>
-        <Route path='/Login' element = {<Login/>}/>
+        <Route element={<AuthLayout/>}>
+          <Route path='/' element={<Signup />} />
+          <Route path='/Login' element={<Login />} />
+        </Route>
+        <Route element={<MainLayout/>}>
+          <Route path='/Dashboard' element = {<Dashboard/>}/>
+        </Route>
       </Routes>
     </main>
   )
