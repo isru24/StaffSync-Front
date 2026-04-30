5f872677bd62830dacc42e3bd00a7f2c009c9528
Israel <israelmandefro165@gmail.com>
2026-04-30 12:43:45 +0300

    modified:   staffsync-front/src/main.jsx

--- staffsync-front/src/main.jsx
+++ staffsync-front/src/main.jsx
@@ -1,13 +1,28 @@
-import App from './App.jsx'
-import ReactDOM from 'react-dom/client';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#414344',
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App/>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode> 
-); 
+  </React.StrictMode>
+);
