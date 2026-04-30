d780b1d74c1882dc7836d7c3e8afb262b26d134b
Israel <israelmandefro165@gmail.com>
2026-04-30 12:44:01 +0300

    modified:   staffsync-front/src/screens/Dashboard.jsx

--- staffsync-front/src/screens/Dashboard.jsx
+++ staffsync-front/src/screens/Dashboard.jsx
@@ -1,17 +1,18 @@
-import { Box, Card, Drawer, Grid, Typography, Divider, List, ListItem, ListItemButton, Link } from '@mui/material'
+import { Box, Card, Drawer, Grid, Typography, Divider, List, ListItem, ListItemButton } from '@mui/material'
+import { Link } from 'react-router-dom';
 
 function StatCard({ title, value, color, trend }) {
     return (
         <Box
             sx={{
-                backgroundColor: "#e5e5e5",
+                backgroundColor: "#7f7f7f",
                 borderRadius: "14px",
                 p: 3,
                 width: '100%',
                 display: 'flex',
                 flexDirection: 'column',
                 justifyContent: 'space-between',
-                margin: '10px'
+                margin: '21px'
             }}
         >
             <Typography variant="body2" sx={{ alignSelf: 'flex-start', mb: 3 }} >{title}</Typography>
@@ -32,8 +33,7 @@ function Dashboard() {
                 '& .MuiDrawer-paper': {
                     width: "15%",
                     boxSizing: 'border-box',
-                    borderRight: '1px solid #e2e8f0',
-                    background: '#d10505',
+                    background: '#7f7f7f',
                     borderRadius: '10px',
                     margin: "5px",
                     height: "97%"
@@ -44,8 +44,8 @@ function Dashboard() {
                         El Matador
                     </Typography>
                 </Box>
-                <Divider sx={{ ml: 2, mr: 2 }} />
-                <List sx={{ px: 2, flexGrow: 1 }}>
+                <Divider sx={{ ml: 2, mr: 2}} />
+                {/* <List sx={{ px: 2, flexGrow: 1 }}>
                     {menuItemClasses.map((item) => {
                         const active = location.pathname === item.path;
                         return (
@@ -57,35 +57,44 @@ function Dashboard() {
                             </ListItem>
                         )
                     })}
-                </List>
+                </List> */}
+                    <p>
+                        <Link to="/Employee">Employee</Link>
+                    </p>
+                    <p>
+                        <Link to="/Attendance">Attendance</Link>
+                    </p>
+                    <p>
+                        <Link to="/Document">Doc</Link>
+                    </p>      
             </Drawer>
-            <Box sx={{ ml: "170px", p: 3, marginTop: '60px' }}>
-                <Grid container spacing={16}>
+            <Box sx={{ ml: "190px", p: 3, marginTop: '60px' }}>
+                <Grid container spacing={12}>
 
                     <Grid item xs={3}>
-                        <StatCard title="Total Employees" value="1000" color="#3b82f6" trend="+2%" />
+                        <StatCard title="Total Employees" value="1000" color="black" trend="+2%" />
                     </Grid>
 
                     <Grid item xs={3}>
-                        <StatCard title="Active Now" value="1,234" color="#8b5cf6" trend="+3.2%" />
+                        <StatCard title="Active Now" value="1,234" color="black" trend="+3.2%" />
                     </Grid>
 
                     <Grid item xs={3}>
-                        <StatCard title="Avg. Session" value="8.4m" color="#10b981" trend="+0.4m" />
+                        <StatCard title="Avg. Session" value="8.4m" color="black" trend="+0.4m" />
                     </Grid>
 
-                    <Grid item xs={3}>
-                        <StatCard title="Care Score" value="98%" color="#f59e0b" trend="Stable" />
+                    <Grid item xs={3} >
+                        <StatCard title="Care Score" value="98%" color="black" trend="Stable"/>
                     </Grid>
                 </Grid>
                 <Grid container spacing={2} >
-                    <Grid item xs={8} sx={{ width: '50%' }}>
-                        <Card style={{ backgroundColor: 'blue', marginLeft: '10px', padding: '10px', height: 270, borderRadius: '10px' }} >
+                    <Grid item xs={4} sx={{ width: '50%' }}>
+                        <Card style={{ backgroundColor: '#7f7f7f', marginLeft: '10px', padding: '10px', height: 270, borderRadius: '10px' }} >
                             Employees
                         </Card>
                     </Grid>
-                    <Grid item xs={4} sx={{ width: '47%' }} >
-                        <Card style={{ backgroundColor: 'pink', marginLeft: '63px', padding: '10px', height: 270, borderRadius: '10px' }} >
+                    <Grid item xs={8} sx={{ width: '48%' }} >
+                    <Card style={{ backgroundColor: '#7f7f7f', padding: '10px', height: 270, borderRadius: '10px' }} >
                             att
                         </Card>
                     </Grid>
